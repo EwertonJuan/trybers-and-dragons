41405dfaf778b2b7bbe5558af995388cb1db2451
Ewerton Juan <juancunha2011@hotmail.com>
2022-11-21 19:04:15 -0300

    refactoring method PVP.fight()

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -12,8 +12,11 @@ export default class PVP extends Battle {
   }
 
   fight(): number {
-    this._player1.attack(this._player2);
-    this._player2.attack(this._player1);
+    while (this._player1.lifePoints > -1 && this._player2.lifePoints > -1) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    }
+
     return super.fight();
   }
 }
